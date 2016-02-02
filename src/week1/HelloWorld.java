cc6c90dd9a0f866fda663a358557f796af3927b7
Tim Hintzbergen <t.a.h.hintzbergen@student.utwente.nl>
2016-02-02 13:44:25 +0100

    first three exercises

---
+++ src/week1/HelloWorld.java
@@ -0,0 +1,11 @@
+package week1;
+
+/**
+ * Created by Kramok on 2/2/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
---
+++ src/week1/Opgave3.java
@@ -0,0 +1,17 @@
+package week1;
+
+/**
+ * Created by Kramok on 2/2/2016.
+ */
+public class Opgave3 {
+    public static void main(String[] args) {
+        int length = 8;
+        int width = 8;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
